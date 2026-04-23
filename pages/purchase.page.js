a520669136fadb235295382f947e8573a482c4f2
MarceloDutra <marcelo.dutra@outlook.com.br>
2026-04-22 22:13:27 -0300

    Concluiu o mapeamento page object purchase.page.js e confirmation.page.js + alteração da feature do modelo simples + preenchimento dos steps da ReservePage

---
+++ pages/purchase.page.js
@@ -0,0 +1,30 @@
+export default class PurchasePage {
+
+    constructor(page){
+        this.page = page
+        this.url = '/purchase\.php/'
+        this.nome = '#inputName' 
+        this.bandeira =  '[name="cardType"]' //'#cardType' ex. '[name="toPort"]'
+        this.remember = '#rememberMe' // ver no simples como usar o getByRole, pegou pelo checkBox
+        this.btn_purchase_flight = '.btn-primary'
+    }
+
+    // como neste mapeamento estamos seguindo o padrão(by the book pageobject), não inclui verificação
+
+    async preencher_nome(nome){
+        await this.page.locator(this.nome).fill(nome)
+    }
+
+    async selecionar_bandeira(bandeira){
+        await this.page.locator(this.bandeira).selectOption(bandeira)
+    }
+
+    async marcar_lembrete(){
+        await this.page.locator(this.remember).check()
+    }
+
+    async comprar_passagem(){
+        await this.page.locator(this.btn_purchase_flight).click()
+    }
+
+}
